9f7fba1a53110aa895feefe974dda148d50dc51f
gbakradze94 <Bakradzegeorge17@gmail.com>
2023-07-28 14:53:00 +0400

    Add and configure cloud packages

--- gateway-service/src/main/java/com/microservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/microservice/GatewayServiceApplication.java
@@ -3,9 +3,15 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
+@EnableFeignClients
 @EnableEurekaClient
 public class GatewayServiceApplication {
     public static void main(String[] args) {
--- resource-service/src/main/java/com/microservice/ResourceServiceApplication.java
+++ resource-service/src/main/java/com/microservice/ResourceServiceApplication.java
@@ -2,8 +2,14 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ResourceServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResourceServiceApplication.class, args);
