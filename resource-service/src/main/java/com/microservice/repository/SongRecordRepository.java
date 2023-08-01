90b230c1bc0064428a68ca8700489cfa9b59d2be
gbakradze94 <Bakradzegeorge17@gmail.com>
2023-08-01 22:45:07 +0400

    Fix project

--- resource-service/src/main/java/com/microservice/repository/SongRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.repository;
-
-import com.microservice.domain.SongRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SongRecordRepository extends JpaRepository<SongRecord, Integer> {
-}
---
+++ song-service/src/main/java/com/microservice/songservice/service/OrganizationService.java
@@ -0,0 +1,8 @@
+package com.microservice.songservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganizationService {
+	
+}
