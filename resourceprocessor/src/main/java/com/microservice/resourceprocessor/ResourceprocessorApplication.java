f6c6d1fb62298f2d31db81dd25499990e28d88c1
gbakradze94 <Bakradzegeorge17@gmail.com>
2023-08-02 13:04:45 +0400

    Add resourceprocessor module

---
+++ resourceprocessor/src/main/java/com/microservice/resourceprocessor/ResourceprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.resourceprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceprocessorApplication.class, args);
+	}
+
+}
